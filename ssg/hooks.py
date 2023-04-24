4af2a7427bb115da02ef5f12fd2fe84801a5c18b
Thuo <89732702+DThuo@users.noreply.github.com>
2023-04-25 09:27:05 +1000

    Completed the project

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -8,7 +8,7 @@ def register(hook, order = 0):
     
     return register_callback
 
-def event(hooks, **args):
+def event(hook, **args):
     for order in sorted(_callbacks.get(hook, {})):
         for func in _callbacks[hook][order]:
             func(*args)
